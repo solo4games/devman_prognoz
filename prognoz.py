068baf0b0c306dcb6ba6a87bcb1bfa9ecfd9c97c
solo4games <78871644+solo4games@users.noreply.github.com>
2023-10-19 12:46:35 +0300

    Update prognoz.py

--- prognoz.py
+++ prognoz.py
@@ -2,8 +2,8 @@ import requests
 
 if __name__ == '__main__':
     resp_svo = requests.get("https://wttr.in/svo?nTqu&lang=ru")
-    resp_paris = requests.get("https://wttr.in/paris?nTqu&lang=ru")
+    resp_paris = requests.get("https://wttr.in/Лондон?nTqu&lang=ru")
     resp_cherepovec = requests.get("https://wttr.in/Череповец?nTqMm&lang=ru")
-    print(resp_svo.text)
     print(resp_paris.text)
+    print(resp_svo.text)
     print(resp_cherepovec.text)
